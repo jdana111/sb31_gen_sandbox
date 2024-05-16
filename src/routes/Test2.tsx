376a9efbfeb2441bec4e4abc70eb3e35c7586ca6
John Dana <john@danasd.com>
2024-05-16 12:49:19 -0600

    Test2: Rearranged the console.log invocations.

--- src/routes/Test2.tsx
+++ src/routes/Test2.tsx
@@ -2,10 +2,11 @@ import { useRef } from "react";
 
 export default function Form() {
   const inputRef = useRef<HTMLInputElement>(null);
+  console.log("INITIAL inputRef.current =", inputRef.current);
   function handleClick() {
     inputRef.current!.focus();
-    console.log("inputRef.current =", inputRef.current);
-    console.log("inputRef.current.value =", inputRef.current!.value);
+    console.log("FUNCTION inputRef.current.value =", inputRef.current!.value);
+    console.log("FUNCTION inputRef.current =", inputRef.current);
   }
 
   return (
