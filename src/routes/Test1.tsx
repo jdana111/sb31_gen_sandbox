75832a5820d02ddbef7d1af14a41918f89ed2442
John Dana <john@danasd.com>
2024-07-15 15:17:16 -0600

    Test1: Changed onMySubmit back to onSubmit.

--- src/routes/Test1.tsx
+++ src/routes/Test1.tsx
@@ -12,14 +12,14 @@ export default function Test1() {
     watch,
     formState: { errors },
   } = useForm<Inputs>()
-  
-  const onMySubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
 
   console.log(watch("example")) // watch input value by passing the name of it
 
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onMySubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       {/* register your input into the hook by invoking the "register" function */}
       <input defaultValue="test" {...register("example")} />
 
