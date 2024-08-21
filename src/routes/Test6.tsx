95a3faf41d1e89f02a0982fd1ff8d0cea1022f70
John Dana <john@danasd.com>
2024-08-21 08:46:27 -0600

    Test6: Experimented with columnVisibilityModel.

--- src/routes/Test6.tsx
+++ src/routes/Test6.tsx
@@ -6,19 +6,23 @@ const rows: GridRowsProp = [
   { id: 3, col1: "Material UI", col2: "is amazing" },
   { id: 4, col1: "MUI", col2: "" },
   { id: 5, col1: "Joy UI", col2: "is awesome" },
-  { id: 6, col1: "MUI Base", col2: "is amazing" }
+  { id: 6, col1: "MUI Base", col2: "is amazing" },
 ];
 
 const columns: GridColDef[] = [
-  { field: "id", hide: true },
+  { field: "id" },
   { field: "col1", headerName: "Column 1", width: 150 },
-  { field: "col2", headerName: "Column 2", width: 150 }
+  { field: "col2", headerName: "Column 2", width: 150 },
 ];
 
 export default function Test6() {
   return (
     <div style={{ height: 300, width: "100%" }}>
-      <DataGrid rows={rows} columns={columns} />
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        columnVisibilityModel={{ id: false }}
+      />
     </div>
   );
 }
