915842f078b261ede6e8ad83d24578aa0da9d60d
John Dana <john@danasd.com>
2024-09-21 06:45:52 -0600

    Test09: Margin collapse experimentation.

--- src/routes/Test09.tsx
+++ src/routes/Test09.tsx
@@ -4,25 +4,38 @@ import React from "react";
 
 const SandboxComponent: React.FC = () => {
   return (
-    <div style={{ backgroundColor: 'lightblue', padding: '10px' }}>
-      <h1 style={{
-        margin: '50px',
-        padding: '30px 0',
-        border: 'solid 1px blue',
-        backgroundColor: '#FF2255',
-      }}>
-        Heading 1
-      </h1>
-      <h2 style={{
-        margin: '20px 0 0 0',
-        border: 'solid 1px blue',
-        backgroundColor: 'red',
-      }}>
-        Heading 2
-      </h2>
-    </div>
+    <>
+      <p>
+        <b>Type 1, Sibling (Peer-Level) Margin Collapse:</b> The margins of adjacent
+        elements overlap, and the larger margin takes precedence.
+      </p>
+      <div style={{ backgroundColor: 'lightblue' }}>
+        <h1
+          style={{
+            margin: '50px',
+            padding: '30px 0',
+            border: 'solid 1px blue',
+            backgroundColor: '#FF2255',
+          }}
+        >
+          Heading 1
+        </h1>
+        <h2
+          style={{
+            margin: '20px 0 0 0',
+            border: 'solid 1px blue',
+            backgroundColor: 'red',
+          }}
+        >
+          Heading 2
+        </h2>
+      </div>
+      <p>
+        <b>Type 2, Parent-Child Margin Collapse:</b> When the parent has no
+        padding/border/content, it "absorbs" the margin of the child.
+      </p>
+    </>
   );
 };
-
 
 export default SandboxComponent;
