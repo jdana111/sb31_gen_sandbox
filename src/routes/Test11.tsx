2d0e65608c7f0334782240e3bc0b06eeb507eb79
John Dana <john@danasd.com>
2024-09-23 07:20:27 -0600

    Test11.tsx: Lots of experimentation with display: block; and display: inline;

--- src/routes/Test11.tsx
+++ src/routes/Test11.tsx
@@ -1,38 +1,25 @@
 // Test11.tsx (Block vs Inline Elements)
 // Source: ChatGPT
-import React from 'react';
+import React from "react";
 
 const Test11: React.FC = () => {
   return (
-    <div style={{ padding: '20px', border: '1px solid black' }}>
-      {/* Block element */}
-      <div
+    <>
+      <span>some text...</span>
+      <p
         style={{
-          display: 'block',
-          backgroundColor: 'lightblue',
-          padding: '10px',
-          margin: '10px',
-          border: '2px solid blue',
-          width: '200px',
-        }}
-      >
-        This is a block element (display: block)
-      </div>
-
-      {/* Inline element */}
-      <div
-        style={{
-          display: 'inline',
-          backgroundColor: 'lightcoral',
-          padding: '10px',
-          margin: '10px',
-          border: '2px solid red',
-          width: '200px', // This will be ignored for inline elements
+          display: "inline",
+          backgroundColor: "lightcoral",
+          padding: "10px",
+          margin: "10px",
+          border: "2px solid red",
+          width: "200px", // This will be ignored for inline elements
         }}
       >
         This is an inline element (display: inline)
-      </div>
-    </div>
+      </p>
+      <span>some more text...</span>
+    </>
   );
 };
 
